d318b778e96ed7eab33bd8d694fb86973e018f5a
shamsad-anam <reeddhy@gmail.com>
2020-07-20 16:01:37 +0600

    added single blog page

--- js/slick-controls.js
+++ js/slick-controls.js
@@ -68,3 +68,41 @@ $(".group-slider-vert").slick({
     // instead of a settings object
   ],
 });
+
+/// ::: cart-slider :::\
+$(".cart-slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: prevBtn,
+  nextArrow: nextBtn,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
